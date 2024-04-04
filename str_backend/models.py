8d4acfa7e899c28866f655271cd344cddb02165d
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-04 01:50:28 +0100

    Updated the models

--- str_backend/models.py
+++ str_backend/models.py
@@ -5,8 +5,8 @@ class Category(models.Model):
     name = models.CharField(max_length=250) 
 
 class Post(models.Model):
-    post_id = models.AutoField(primary_key=True)
-    category = models.ForeignKey(Category, related_name='posts', on_delete=models.PROTECT)
+    post_id = models.AutoField(primary_key=True, default=0)
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.PROTECT, default=0)
     title = models.CharField(max_length=200)
     img_url = models.CharField(max_length=200, null=True)
     author = models.CharField(max_length=50, default='')
@@ -16,12 +16,12 @@ class Post(models.Model):
     published_at = models.DateTimeField(auto_now_add=True)
 
 class PostViews(models.Model):
-    post_view_id = models.AutoField(primary_key=True)
-    post = models.OneToOneField(Post, related_name='views', on_delete=models.CASCADE)
+    post_view_id = models.AutoField(primary_key=True, default=0)
+    post = models.OneToOneField(Post, related_name='views', on_delete=models.CASCADE, default=0)
     total_views = models.PositiveIntegerField(default=0)
 
 class Comment(models.Model):
-    comment_id = models.AutoField(primary_key=True)
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    content = models.TextField()
+    comment_id = models.AutoField(primary_key=True, default=0)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE, default=0)
+    content = models.TextField(default='')
     created_at = models.DateTimeField(auto_now_add=True)
