4499dc2b40b60879b1f2268e17f3e3c782e33f75
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-04 02:03:18 +0100

    Updated the site

--- str_backend/admin.py
+++ str_backend/admin.py
@@ -1,5 +1,23 @@
 # admin.py
 from django.contrib import admin
-from .models import Category
+from .models import Category, Post, PostViews, Comment
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_id', 'name')
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('post_id', 'category', 'title', 'author', 'published_at')
+    list_filter = ('category', 'author')
+    search_fields = ('title', 'author')
+
+@admin.register(PostViews)
+class PostViewsAdmin(admin.ModelAdmin):
+    list_display = ('post_view_id', 'post', 'total_views')
+    search_fields = ('post__title',)  # Use double underscore to navigate relationships
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('comment_id', 'post', 'content', 'created_at')
+    search_fields = ('post__title', 'content')  # Use double underscore to navigate relationships
--- str_backend/models.py
+++ str_backend/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=250) 
+    name = models.CharField(max_length=250)
+    img_url = models.CharField(max_length=250, null=True)
 
     def __str__(self):
         return self.name
@@ -16,15 +17,25 @@ class Post(models.Model):
     summary = models.CharField(max_length=200, null=True)
     content = models.TextField(default='')
     slug = models.CharField(max_length=50, default='')
+    tags = models.CharField(max_length=200, null=True)
     published_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 class PostViews(models.Model):
     post_view_id = models.AutoField(primary_key=True, default=0)
     post = models.OneToOneField(Post, related_name='views', on_delete=models.CASCADE, default=0)
     total_views = models.PositiveIntegerField(default=0)
 
+    def __str__(self):
+        return self.post
+
 class Comment(models.Model):
     comment_id = models.AutoField(primary_key=True, default=0)
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE, default=0)
     content = models.TextField(default='')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
