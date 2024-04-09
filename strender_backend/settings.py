7bc65a538d687d872fa69f8c88b6f09b4a33d358
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-09 21:47:30 +0100

    Updated the settings.py

--- strender_backend/settings.py
+++ strender_backend/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i90*^dk-0)bp5lo+ph3qww8c_+6yd%$b#2fa+kc(v*l9owg(jh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 environ.Env.read_env()
 
@@ -56,6 +56,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'https://strender.vercel.app',
+    '.onrender.com',
+    '.vercel.app',
 ]
 
 ROOT_URLCONF = 'strender_backend.urls'
